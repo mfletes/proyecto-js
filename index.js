ddbcaf174cbe96d760c4a55e9e57d124dfc54d23
Mario Fletes <mfletes@gmail.com>
2024-01-03 11:07:32 -0600

    modificando

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fn = () => {
-    const miString = "soy un string!"
-    let b = miString + " muy feliz"
+    const miString = "soy una cadena!"
+    let b = miString + " muy feliz."
     console.log(b)
 }
 
@@ -8,6 +8,5 @@ fn()
 
 const clickeado = () => {
     let a = "chanchito feliz"
-
     console.log(a)
 }
